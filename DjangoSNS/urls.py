76d1e0726a4367c06059077330feb6074bd0c311
龍田 佳樹 <yo-tatsuta@a.tgl.co.jp>
2023-07-12 10:53:02 +0900

    20230712

--- DjangoSNS/urls.py
+++ DjangoSNS/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
-from snsbackend.views import SnsPostViewSet, LoginAPIView
-from django.views.decorators.csrf import get_token
-from django.http import JsonResponse
+from snsbackend.views import SnsPostViewSet, LoginAPIView, create_user
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -13,4 +11,5 @@ router.register(r'snsposts', SnsPostViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('api/login/', LoginAPIView.as_view(), name='login'),
+    path('api/signup/', create_user, name='signup'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- snsbackend/views.py
+++ snsbackend/views.py
@@ -3,9 +3,7 @@ from rest_framework import viewsets
 from .models import SnsPost, UserProfile
 from .serializers import SnsPostSerializer
 from django.http import JsonResponse
-from django.contrib.auth import authenticate, login
-# from rest_framework_jwt.views import ObtainJSONWebToken
-# from rest_framework.response import Response
+from django.contrib.auth import authenticate,login
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import View
@@ -14,22 +12,7 @@ from django.contrib.auth.models import User
 class SnsPostViewSet(viewsets.ModelViewSet):
     queryset = SnsPost.objects.all()
     serializer_class = SnsPostSerializer
-    
 
-# class CustomObtainJSONWebToken(ObtainJSONWebToken):
-#     def post(self, request, *args, **kwargs):
-#         username = request.data.get('username')
-#         password = request.data.get('password')
-
-#         user = authenticate(username=username, password=password)
-#         if user:
-#             response = super().post(request, *args, **kwargs)
-#             # ログインが成功した場合に追加の情報をレスポンスに含める場合は、ここで処理を追加します
-#             return response
-
-#         return Response({'detail': 'Invalid credentials'}, status=400)
-
-# login_view = CustomObtainJSONWebToken.as_view()
 
 @method_decorator(csrf_exempt, name='dispatch')
 class LoginAPIView(View):
@@ -38,9 +21,26 @@ class LoginAPIView(View):
         params = json.loads(request.body)
         username = params['username']
         password = params['password']
-        user = User.objects.get(username=username, password=password)
+        user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             return JsonResponse({'is_authenticated': True, 'user_id': user.id})
 
         return JsonResponse({'is_authenticated': False}, status=403)
+
+
+@csrf_exempt  # CSRF保護を無効化（開発時のみ）
+def create_user(request):
+    if request.method == 'POST':
+        params = json.loads(request.body)
+        username = params.get('username')
+        password = params.get('password')
+
+        if username and password:
+            user = User.objects.create_user(username=username, password=password)
+            return JsonResponse({'message': 'User created successfully.'})
+        else:
+            return JsonResponse({'message': 'Username and password are required.'}, status=400)
+    else:
+        return JsonResponse({'message': 'Invalid request method.'}, status=405)
+
